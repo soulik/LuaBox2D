c62108ab8f371fff859eae304206fc9615adf18f
Mário Kašuba <mario.kasuba@it-academy.sk>
2014-10-12 22:40:45 +0200

    Added Box2D dependency Prepared skeleton code for library

---
+++ common.hpp
@@ -0,0 +1,7 @@
+#ifndef LUABOX2D_COMMON_H
+#define LUABOX2D_COMMON_H
+
+#include <Lua/lua.hpp>
+#include <Box2D/Box2d.h>
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "common.hpp"
+
+extern "C" LUA_API int luaopen_Box2D(void * current_state){
+
+	return 1;
+}
